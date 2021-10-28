747c25a152c194082d7d9ba321f5d194e43483a6
Kubaryt <88268063+Kubaryt@users.noreply.github.com>
2021-10-28 08:35:44 +0200

    Update kalkulator3.py

--- kalkulator3.py
+++ kalkulator3.py
@@ -1,4 +1,16 @@
 import math
+'''
+import QtQuick 2.3
+import QtQuick.Controls 1.4
+'''
+
+import sys
+
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QLabel
+from PyQt5.QtWidgets import QWidget
+app = QApplication(sys.argv)
+
 DZIALANIA = 8
 
 
@@ -22,17 +34,17 @@ if DZIALANIA <= 3:
 if DZIALANIA == 0:
 
     c = a + b
-    print('Wynik dodawania to: %f' %  c )
+    #print('Wynik dodawania to: %f' %  c )
 
 if DZIALANIA == 1:
 
     c = a - b
-    print('Wynik odejmowania to: %f' %  c )
+    #print('Wynik odejmowania to: %f' %  c )
 
 if DZIALANIA == 2:
 
     c = a * b
-    print('Wynik mnozenia to: %f' %  c )
+    #print('Wynik mnozenia to: %f' %  c )
     
 if DZIALANIA == 3:
 
@@ -41,12 +53,12 @@ if DZIALANIA == 3:
         b = float(input('Wpisz druga liczbe '))
     
     c = a / b
-    print('Wynik dzielenia to: %f' % c )
+    #print('Wynik dzielenia to: %f' % c )
 
 
 #WPROWADZANIE LICZBY (4-6)
 
-if DZIALANIA >= 4 & DZIALANIA != 7:
+if DZIALANIA >= 4 and DZIALANIA != 7:
 
     a = float(input('Wprowadz pierwsza liczbe '))
 
@@ -56,17 +68,17 @@ if DZIALANIA >= 4 & DZIALANIA != 7:
 if DZIALANIA == 4:
 
     c = a ** b
-    print('Wynik potegowania to: %f' % c)
+    #print('Wynik potegowania to: %f' % c)
 
 if DZIALANIA == 5:
 
     c = math.log( a )
-    print('Wynik to: %f' % c)
+    #print('Wynik to: %f' % c)
 
 if DZIALANIA == 6:
     
     c = math.sqrt( a )
-    print('Wynik to %f' % c)
+    #print('Wynik to %f' % c)
 
 
 #SREDNIA ARYTMETYCZNA
@@ -90,4 +102,14 @@ if DZIALANIA == 7:
         i = i - 1
     
     c = wynik / b
-    print('Wynik to: %f' % c)
+    #print('Wynik to: %f' % c)
+
+#WYSWIETLANIE WYNIKU
+window = QWidget()
+window.setWindowTitle('PyQt5 App')
+window.setGeometry(150, 150, 330, 130)
+window.move(60, 15)
+WynikMsg = QLabel('<h1>Wynik to: %f</h1>' % c, parent=window)
+WynikMsg.move(30, 50)
+window.show()
+sys.exit(app.exec_())
