230004280e2a925e8b879d8b85da517cb9f3f456
Kubaryt <88268063+Kubaryt@users.noreply.github.com>
2021-11-15 19:30:53 +0100

    Update kalkulator3.py

--- kalkulator3.py
+++ kalkulator3.py
@@ -1,111 +1,126 @@
 import math
-
+'''
+import QtQuick 2.3
+import QtQuick.Controls 1.4
+'''
+'''
 import sys
 
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QLabel
 from PyQt5.QtWidgets import QWidget
 app = QApplication(sys.argv)
+'''
 
-DZIALANIA = 8
+loop = 1
 
+while loop:
 
-#WYBOR DZIALANIA
-
-while DZIALANIA >= 8:
-
-    DZIALANIA = int(input('Wybierz dzialanie: 0 - Dodawanie | 1 - Odejmowanie | 2 - Mnozenie | 3 - Dzielenie | 4 - Potegowanie | 5 - Logarytm naturalny | 6 - Pierwiastek kwadratowy | 7 - Średnia arytmetyczna '))
+    DZIALANIA = 8
 
 
-#WPROWADZANIE LICZB (0-3)
+    #WYBOR DZIALANIA
 
-if DZIALANIA <= 3:
-
-    a = float(input('Wprowadz pierwsza liczbe '))
-    b = float(input('Wprowadz druga liczbe '))
-
-
-#WYPROWADZANIE WYNIKU (0-3)
-
-if DZIALANIA == 0:
-
-    c = a + b
-    #print('Wynik dodawania to: %f' %  c )
-
-if DZIALANIA == 1:
-
-    c = a - b
-    #print('Wynik odejmowania to: %f' %  c )
-
-if DZIALANIA == 2:
-
-    c = a * b
-    #print('Wynik mnozenia to: %f' %  c )
+    while DZIALANIA >= 8:
     
-if DZIALANIA == 3:
+        DZIALANIA = int(input('Wybierz dzialanie: 0 - Dodawanie | 1 - Odejmowanie | 2 - Mnozenie | 3 - Dzielenie | 4 - Potegowanie | 5 - Logarytm naturalny | 6 - Pierwiastek kwadratowy | 7 - Średnia arytmetyczna '))
 
-    while b == 0:
+
+    #WPROWADZANIE LICZB (0-3)
+
+    if DZIALANIA <= 3:
+
+        a = float(input('Wprowadz pierwsza liczbe '))
+        b = float(input('Wprowadz druga liczbe '))
+
+
+    #WYPROWADZANIE WYNIKU (0-3)
+
+    if DZIALANIA == 0:
+
+        c = a + b
+        print('Wynik dodawania to: %f' %  c )
+
+    if DZIALANIA == 1:
+
+        c = a - b
+        print('Wynik odejmowania to: %f' %  c )
+
+    if DZIALANIA == 2:
+
+        c = a * b
+        print('Wynik mnozenia to: %f' %  c )
     
-        b = float(input('Wpisz druga liczbe '))
+    if DZIALANIA == 3:
+
+        while b == 0:
     
-    c = a / b
-    #print('Wynik dzielenia to: %f' % c )
-
-
-#WPROWADZANIE LICZBY (4-6)
-
-if DZIALANIA >= 4 and DZIALANIA != 7:
-
-    a = float(input('Wprowadz pierwsza liczbe '))
-
-
-#WYPROWADZANIE WYNIKU (0-3)
-
-if DZIALANIA == 4:
-
-    c = a ** b
-    #print('Wynik potegowania to: %f' % c)
-
-if DZIALANIA == 5:
-
-    c = math.log( a )
-    #print('Wynik to: %f' % c)
-
-if DZIALANIA == 6:
+            b = float(input('Wpisz druga liczbe '))
     
-    c = math.sqrt( a )
-    #print('Wynik to %f' % c)
+        c = a / b
+        print('Wynik dzielenia to: %f' % c )
 
 
-#SREDNIA ARYTMETYCZNA
+    #WPROWADZANIE LICZBY (4-6)
 
-if DZIALANIA == 7:
+    if DZIALANIA >= 4 and DZIALANIA != 7:
 
-    b = 0
-    while b == 0:
+        a = float(input('Wprowadz pierwsza liczbe '))
 
-        b = int(input('Wprowadz ilosc liczb '))
+
+    #WYPROWADZANIE WYNIKU (0-3)
+
+    if DZIALANIA == 4:
+
+        c = a ** b
+        print('Wynik potegowania to: %f' % c)
+
+    if DZIALANIA == 5:
+
+        c = math.log( a )
+        print('Wynik to: %f' % c)
+
+    if DZIALANIA == 6:
     
-    i = b
-    r = 1
-    wynik = 0
+        c = math.sqrt( a )
+        print('Wynik to %f' % c)
 
-    while i >= 1:
 
-        a = float(input('Wprowadz liczbe po raz %g ' % r))
-        wynik = wynik + a
-        r = r + 1
-        i = i - 1
+    #SREDNIA ARYTMETYCZNA
+
+    if DZIALANIA == 7:
+
+        b = 0
+        while b == 0:
+
+            b = int(input('Wprowadz ilosc liczb '))
     
-    c = wynik / b
-    #print('Wynik to: %f' % c)
+        i = b
+        r = 1
+        wynik = 0
 
-#WYSWIETLANIE WYNIKU
-window = QWidget()
-window.setWindowTitle('PyQt5 App')
-window.setGeometry(150, 150, 330, 130)
-window.move(60, 15)
-WynikMsg = QLabel('<h1>Wynik to: %f</h1>' % c, parent=window)
-WynikMsg.move(30, 50)
-window.show()
-sys.exit(app.exec_())
+        while i >= 1:
+
+            a = float(input('Wprowadz liczbe po raz %g ' % r))
+            wynik = wynik + a
+            r = r + 1
+            i = i - 1
+    
+        c = wynik / b
+        print('Wynik to: %f' % c)
+
+    #WYSWIETLANIE WYNIKU
+    '''
+    window = QWidget()
+    window.setWindowTitle('PyQt5 App')
+    window.setGeometry(150, 150, 330, 130)
+    window.move(60, 15)
+    WynikMsg = QLabel('<h1>Wynik to: %f</h1>' % c, parent=window)
+    WynikMsg.move(30, 50)
+    window.show()
+    sys.exit(app.exec_())
+    '''
+
+    #POWTARZANIE
+
+    loop = int(input('Aby zakończyć powtarzanie kodu wpisz 0: '))
