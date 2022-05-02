7ae51845115f352517dd78de278e5c712b4bdc4b
Kubaryt <88268063+Kubaryt@users.noreply.github.com>
2022-05-02 19:47:45 +0200

    Delete podrecznik1.py

--- podrecznik1.py
+++
@@ -1,9 +0,0 @@
-print('Obliczenie pola i obwodu prostokata\n')
-
-a = float(input('Podaj dlugosc pierwszego boku '))
-b = float(input('Podaj dlugosc drugiego boku '))
-
-p = a * b
-o = a * 2 + b * 2
-
-print('\nPole prostokata wynosi: %f, a obwod: %f' % (p, o))
